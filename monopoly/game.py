dfff033a51d5a672e49124ac7d2b24de3bde984a
Stephen Curtis <steve.curtis106@gmail.com>
2025-02-19 23:53:30 -0700

    Added space objects

--- monopoly/game.py
+++ monopoly/game.py
@@ -1,16 +1,62 @@
 import random
+from space import Go, Property, CommunityChest, Chance, Jail, Utility, Railroad, Tax, FreeParking, GoToJail
 
 class Game:
     def __init__(self, players):
         self.players = players
         self.current_player_index = 0
         self.determine_turn_order()
+        self.create_board()
 
     # Whoever rolls the highest goes first
     def determine_turn_order(self):
         rolls = {player: self.roll_dice(player) for player in self.players}
         self.players = [player for player, _ in sorted(rolls.items(), key=lambda x: x[1], reverse=True)]
         print(f'\n{self.players[0]} goes first!')
+
+    def create_board(self):
+        self.board = [
+            Go('Go'),
+            Property('Mediterranean Avenue', 60, 2, 10, 30, 90, 160, 250, 50, 'brown'),
+            CommunityChest('Community Chest', 'Advance to Go', 'advance_to_go'),
+            Property('Baltic Avenue', 60, 4, 20, 60, 180, 320, 450, 50, 'brown'),
+            Tax('Income Tax', 200, 0),
+            Railroad('Reading Railroad', 200, 25),
+            Property('Oriental Avenue', 100, 6, 30, 90, 270, 400, 550, 50, 'light blue'),
+            Chance('Chance', 'Advance to Go', 'advance_to_go'),
+            Property('Vermont Avenue', 100, 6, 30, 90, 270, 400, 550, 50, 'light blue'),
+            Property('Connecticut Avenue', 120, 8, 40, 100, 300, 450, 600, 50, 'light blue'),
+            Jail('Jail'),
+            Property('St. Charles Place', 140, 10, 50, 150, 450, 625, 750, 100, 'pink'),
+            Utility('Electric Company', 150, 'electric_company'),
+            Property('States Avenue', 140, 10, 50, 150, 450, 625, 750, 100, 'pink'),
+            Property('Virginia Avenue', 160, 12, 60, 180, 500, 700, 900, 100, 'pink'),
+            Railroad('Pennsylvania Railroad', 200, 25),
+            Property('St. James Place', 180, 14, 70, 200, 550, 750, 950, 100, 'orange'),
+            CommunityChest('Community Chest', 'Advance to Go', 'advance_to_go'),
+            Property('Tennessee Avenue', 180, 14, 70, 200, 550, 750, 950, 100, 'orange'),
+            Property('New York Avenue', 200, 16, 80, 220, 600, 800, 1000, 100, 'orange'),
+            FreeParking('Free Parking'),
+            Property('Kentucky Avenue', 220, 18, 90, 250, 700, 875, 1050, 150, 'red'),
+            Chance('Chance', 'Advance to Illinois Avenue', 'advance_to_illinois'),
+            Property('Indiana Avenue', 220, 18, 90, 250, 700, 875, 1050, 150, 'red'),
+            Property('Illinois Avenue', 240, 20, 100, 300, 750, 925, 1100, 150, 'red'),
+            Railroad('B. & O. Railroad', 200, 25),
+            Property('Atlantic Avenue', 260, 22, 110, 330, 800, 975, 1150, 150, 'yellow'),
+            Property('Ventnor Avenue', 260, 22, 110, 330, 800, 975, 1150, 150, 'yellow'),
+            Utility('Water Works', 150, 'water_works'),
+            Property('Marvin Gardens', 280, 24, 120, 360, 850, 1025, 1200, 150, 'yellow'),
+            GoToJail('Go to Jail'),
+            Property('Pacific Avenue', 300, 26, 130, 390, 900, 1100, 1275, 200, 'green'),
+            Property('North Carolina Avenue', 300, 26, 130, 390, 900, 1100, 1275, 200, 'green'),
+            CommunityChest('Community Chest', 'Advance to Boardwalk', 'advance_to_boardwalk'),
+            Property('Pennsylvania Avenue', 320, 28, 150, 450, 1000, 1200, 1400, 200, 'green'),
+            Railroad('Short Line', 200, 25),
+            Chance('Chance', 'Advance to Boardwalk', 'advance_to_boardwalk'),
+            Property('Park Place', 350, 35, 175, 500, 1100, 1300, 1500, 200, 'dark blue'),
+            Tax('Luxury Tax', 75, 0),
+            Property('Boardwalk', 400, 50, 200, 600, 1400, 1700, 2000, 200, 'dark blue')
+        ]
 
     # Rolls 2 six-sided dice
     def roll_dice(self, player):
@@ -29,14 +75,14 @@ class Game:
     def play(self):
         while True:
             double_count = 0
+            roll = None
             player = self.players[self.current_player_index]
             print(f'\n{player}\'s turn!')
             while double_count < 3:
                 (roll, is_double) = self.roll_dice(player)
                 if is_double:
                     player.position = (player.position + roll) % 40
-                    print(f'You landed on {player.position}')
-                    print(f'You rolled a double! Go again')
+                    print(f'{player} landed on {self.board[player.position]}. Go again!')
                     double_count += 1
                 else:
                     break
@@ -47,6 +93,7 @@ class Game:
                 print(f'{player} rolled 3 doubles in a row - go to jail')
             else:
                 player.position = (player.position + roll) % 40
-                print(f'{player} landed on {player.position}')
+                print(f'{player} landed on {self.board[player.position]}')
+
             
             self.current_player_index = (self.current_player_index + 1) % len(self.players)
---
+++ monopoly/space.py
@@ -0,0 +1,80 @@
+class Space():
+    def __init__(self, name):
+        self.name = name
+        
+    def __str__(self):
+        return self.name
+
+
+class Property(Space):
+    def __init__(self, name, price, rent_lvl0, rent_lvl1, rent_lvl2, rent_lvl3, rent_lvl4, rent_lvl5, house_cost, color_group):
+        super().__init__(name)
+        self.price = price
+        self.rent_lvl0 = rent_lvl0
+        self.rent_lvl1 = rent_lvl1
+        self.rent_lvl2 = rent_lvl2
+        self.rent_lvl3 = rent_lvl3
+        self.rent_lvl4 = rent_lvl4
+        self.rent_lvl5 = rent_lvl5
+        self.house_cost = house_cost
+        self.color_group = color_group
+        self.owner = None
+        self.houses = 0
+        self.mortgaged = False
+
+
+class Jail(Space):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Go(Space):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class FreeParking(Space):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class GoToJail(Space):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Chance(Space):
+    def __init__(self, name, description, action):
+        super().__init__(name)
+        self.description = description
+        self.action = action
+
+
+class CommunityChest(Space):
+    def __init__(self, name, description, action):
+        super().__init__(name)
+        self.description = description
+        self.action = action
+
+
+class Utility(Space):
+    def __init__(self, name, price, action):
+        super().__init__(name)
+        self.price = price
+        self.action = action
+        self.owner = None
+
+
+class Railroad(Space):
+    def __init__(self, name, price, rent):
+        super().__init__(name)
+        self.price = price
+        self.rent = rent
+        self.owner = None
+
+
+class Tax(Space):
+    def __init__(self, name, amount, percentage):
+        super().__init__(name)
+        self.amount = amount
+        self.percentage = percentage
